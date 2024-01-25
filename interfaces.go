6bec9f45f0e98c2ffa16da5a5c5ba94b97671741
Bart <bartventer@outlook.com>
2024-01-25 08:35:48 +0000

    fix: update migrator alias, add fossa workflow

--- interfaces.go
+++ interfaces.go
@@ -2,8 +2,6 @@ package multitenancy
 
 import (
 	"gorm.io/gorm"
-	"gorm.io/gorm/clause"
-	"gorm.io/gorm/schema"
 )
 
 // TenantTabler is the interface for tenant tables
@@ -12,43 +10,7 @@ type TenantTabler interface {
 	IsTenantTable() bool
 }
 
-// Migrator is the interface for the migrator (as defined in gorm: https://gorm.io/docs/migration.html#Migrator-Interface)
-type Migrator interface {
-	// AutoMigrate
-	AutoMigrate(dst ...interface{}) error
-
-	// Database
-	CurrentDatabase() string
-	FullDataTypeOf(*schema.Field) clause.Expr
-
-	// Tables
-	CreateTable(dst ...interface{}) error
-	DropTable(dst ...interface{}) error
-	HasTable(dst interface{}) bool
-	RenameTable(oldName, newName interface{}) error
-	GetTables() (tableList []string, err error)
-
-	// Columns
-	AddColumn(dst interface{}, field string) error
-	DropColumn(dst interface{}, field string) error
-	AlterColumn(dst interface{}, field string) error
-	MigrateColumn(dst interface{}, field *schema.Field, columnType gorm.ColumnType) error
-	HasColumn(dst interface{}, field string) bool
-	RenameColumn(dst interface{}, oldName, field string) error
-	ColumnTypes(dst interface{}) ([]gorm.ColumnType, error)
-
-	// Views
-	CreateView(name string, option gorm.ViewOption) error
-	DropView(name string) error
-
-	// Constraints
-	CreateConstraint(dst interface{}, name string) error
-	DropConstraint(dst interface{}, name string) error
-	HasConstraint(dst interface{}, name string) bool
-
-	// Indexes
-	CreateIndex(dst interface{}, name string) error
-	DropIndex(dst interface{}, name string) error
-	HasIndex(dst interface{}, name string) bool
-	RenameIndex(dst interface{}, oldName, newName string) error
-}
+// Migrator is an alias for [gorm.Migrator].
+//
+// [gorm.Migrator]: https://pkg.go.dev/gorm.io/gorm#Migrator
+type Migrator = gorm.Migrator
