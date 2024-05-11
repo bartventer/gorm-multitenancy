4c39b5e22390a1ddfafda25fabdbe5f23d81056f
Bart Venter <72999113+bartventer@users.noreply.github.com>
2024-05-11 19:05:04 +0000

    refactor(internal/testutil): Add WithDBName option to GetDSN function
    This commit refactors the `GetDSN` function in the `testutil` package to accept an optional `WithDBName` option. This option allows the caller to set the database name for the connection. The function now checks if the `dbname` parameter already exists in the DSN string and replaces it if found. If the `dbname` parameter does not exist, it appends it to the DSN string. This change provides more flexibility in configuring the test database connection.

--- internal/testutil/internal.go
+++ internal/testutil/internal.go
@@ -4,26 +4,57 @@ package testutil
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/pkg/errors"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
+var (
+	dbnamePrefix = "dbname="
+)
+
+type DSNOption func(string) string
+
+// WithDBName sets the database name for the connection.
+func WithDBName(name string) DSNOption {
+	return func(dsn string) string {
+		if strings.Contains(dsn, dbnamePrefix) {
+			// Replace existing dbname
+			start := strings.Index(dsn, dbnamePrefix) + len(dbnamePrefix)
+			end := strings.Index(dsn[start:], " ")
+			if end == -1 { // If dbname is the last parameter
+				end = len(dsn)
+			} else {
+				end += start
+			}
+			return dsn[:start] + name + dsn[end:]
+		}
+		// Append dbname if it doesn't exist
+		return fmt.Sprintf("%s %s%s", dsn, dbnamePrefix, name)
+	}
+}
+
 // GetDSN returns the data source name for the database connection.
-func GetDSN() string {
-	return fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable",
-		os.Getenv("POSTGRES_HOST"),
-		os.Getenv("POSTGRES_PORT"),
-		os.Getenv("POSTGRES_USER"),
-		os.Getenv("POSTGRES_DB"),
-		os.Getenv("POSTGRES_PASSWORD"),
+func GetDSN(opts ...DSNOption) string {
+	dsn := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PASSWORD"),
 	)
+	for _, opt := range opts {
+		dsn = opt(dsn)
+	}
+	return dsn
 }
 
 // NewTestDB creates a new database connection (for internal use).
-func NewTestDB() *gorm.DB {
-	db, err := gorm.Open(postgres.Open(GetDSN()), &gorm.Config{PrepareStmt: true})
+func NewTestDB(opts ...DSNOption) *gorm.DB {
+	dsn := GetDSN(opts...)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{PrepareStmt: true})
 	if err != nil {
 		panic(errors.Wrap(err, "failed to connect to test database"))
 	}
