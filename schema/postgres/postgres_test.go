335dad6e255f3b7197b5239eb35892ee45711f38
Bart Venter <72999113+bartventer@users.noreply.github.com>
2024-05-11 19:11:44 +0000

    test(schema/postgres): update SetSearchPath benchmarks

--- schema/postgres/postgres_test.go
+++ schema/postgres/postgres_test.go
@@ -1,53 +1,17 @@
-package postgres
+package postgres_test
 
 import (
 	"fmt"
 	"testing"
 
+	pgdriver "github.com/bartventer/gorm-multitenancy/v5/drivers/postgres"
 	"github.com/bartventer/gorm-multitenancy/v5/internal/testutil"
+	pgschema "github.com/bartventer/gorm-multitenancy/v5/schema/postgres"
+	scopes "github.com/bartventer/gorm-multitenancy/v5/scopes"
 	"gorm.io/gorm"
 	"gorm.io/gorm/clause"
 )
 
-func Test_getSchemaNameFromSqlExpr(t *testing.T) {
-	type args struct {
-		tableExprSQL string
-	}
-	tests := []struct {
-		name string
-		args args
-		want string
-	}{
-		{
-			name: "Test with schema and table",
-			args: args{
-				tableExprSQL: "\"test_domain\".\"mock_private\"",
-			},
-			want: "test_domain",
-		},
-		{
-			name: "Test with only table",
-			args: args{
-				tableExprSQL: "\"mock_private\"",
-			},
-			want: "",
-		},
-		{
-			name: "Test with empty string",
-			args: args{
-				tableExprSQL: "",
-			},
-			want: "",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := getSchemaNameFromSQLExpr(tt.args.tableExprSQL); got != tt.want {
-				t.Errorf("getSchemaNameFromTableExpreSql() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
 func TestGetSchemaNameFromDb(t *testing.T) {
 	type args struct {
 		tx *gorm.DB
@@ -85,7 +49,7 @@ func TestGetSchemaNameFromDb(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := GetSchemaNameFromDb(tt.args.tx)
+			got, err := pgschema.GetSchemaNameFromDb(tt.args.tx)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("GetSchemaNameFromDb() error = %v, wantErr %v", err, tt.wantErr)
 				return
@@ -99,7 +63,7 @@ func TestGetSchemaNameFromDb(t *testing.T) {
 
 func TestSetSearchPath(t *testing.T) {
 	// Connect to the test database.
-	db := testutil.NewTestDB()
+	db := testutil.NewTestDB(testutil.WithDBName("tenants1"))
 
 	schema := "domain1"
 	// create a new schema if it does not exist
@@ -110,7 +74,7 @@ func TestSetSearchPath(t *testing.T) {
 	defer db.Exec(fmt.Sprintf("DROP SCHEMA IF EXISTS %s CASCADE", schema))
 
 	// Test SetSearchPath with a valid schema name.
-	db, reset := SetSearchPath(db, schema)
+	db, reset := pgschema.SetSearchPath(db, schema)
 	if err = db.Error; err != nil {
 		t.Errorf("SetSearchPath() with valid schema name failed, expected %v, got %v", nil, err)
 	}
@@ -122,45 +86,75 @@ func TestSetSearchPath(t *testing.T) {
 	}
 
 	// Test SetSearchPath with an empty schema name.
-	db, _ = SetSearchPath(db, "")
+	db, _ = pgschema.SetSearchPath(db, "")
 	if err = db.Error; err == nil {
 		t.Errorf("SetSearchPath() with empty schema name did not fail, expected an error, got %v", err)
 	}
 }
 
+type Tenant struct {
+	pgdriver.TenantPKModel
+}
+
+func (Tenant) TableName() string { return "public.tenants" }
+
+type Book struct {
+	gorm.Model
+	TenantID string `gorm:"column:tenant_schema"`
+	Tenant   Tenant `gorm:"foreignKey:TenantID;references:SchemaName"`
+}
+
+func (Book) TableName() string   { return "books" }
+func (Book) IsTenantTable() bool { return true }
+
 func BenchmarkSetSearchPath(b *testing.B) {
-	// Connect to the test database.
-	db := testutil.NewTestDB()
-
-	schema := "domain1"
-	// create a new schema if it does not exist
-	err := db.Exec(fmt.Sprintf("CREATE SCHEMA IF NOT EXISTS %s", schema)).Error
+	db, err := gorm.Open(pgdriver.Open(testutil.GetDSN(testutil.WithDBName("tenants2"))), &gorm.Config{})
 	if err != nil {
-		b.Errorf("Create schema failed, expected %v, got %v", nil, err)
+		b.Fatalf("Open failed: %v", err)
 	}
-	defer db.Exec(fmt.Sprintf("DROP SCHEMA IF EXISTS %s CASCADE", schema))
+	if err := pgdriver.RegisterModels(db, &Tenant{}, &Book{}); err != nil {
+		b.Fatalf("RegisterModels failed: %v", err)
+	}
+	if err := pgdriver.MigratePublicSchema(db); err != nil {
+		b.Fatalf("Migrate public schema failed: %v", err)
+	}
+	tenant := Tenant{
+		TenantPKModel: pgdriver.TenantPKModel{
+			DomainURL:  "tenant1.example.com",
+			SchemaName: "tenant1",
+		},
+	}
+	if err := db.Clauses(clause.OnConflict{DoNothing: true}).Create(&tenant).Error; err != nil {
+		b.Fatalf("Create tenant failed: %v", err)
+	}
+	if err := pgdriver.CreateSchemaForTenant(db, tenant.SchemaName); err != nil {
+		b.Fatalf("Create schema for tenant failed: %v", err)
+	}
+	defer pgdriver.DropSchemaForTenant(db, tenant.SchemaName)
 
-	// Benchmark SetSearchPath with a valid schema name.
-	b.Run("SetSearchPath", func(b *testing.B) {
+	b.ResetTimer()
+	b.Run("Create with WithTenantSchema", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
-			db, _ = SetSearchPath(db, schema)
-			if err = db.Error; err != nil {
-				b.Errorf("SetSearchPath() with valid schema name failed, expected %v, got %v", nil, err)
+			book := Book{
+				Tenant: tenant,
+			}
+			if err := db.Scopes(scopes.WithTenantSchema(tenant.SchemaName)).Create(&book).Error; err != nil {
+				b.Errorf("Create book failed, expected %v, got %v", nil, err)
 			}
 		}
 	})
-
-	// Benchmark ResetSearchPath.
-	b.Run("ResetSearchPath", func(b *testing.B) {
+	b.Run("Create with SetSearchPath", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
-			var reset ResetSearchPath
-			db, reset = SetSearchPath(db, schema)
-			if err = db.Error; err != nil {
+			_, resetSearchPath := pgschema.SetSearchPath(db, tenant.SchemaName)
+			if err := db.Error; err != nil {
 				b.Errorf("SetSearchPath() with valid schema name failed, expected %v, got %v", nil, err)
 			}
-			err = reset()
-			if err != nil {
-				b.Errorf("ResetSearchPath() failed, expected %v, got %v", nil, err)
+			defer resetSearchPath()
+			book := Book{
+				Tenant: tenant,
+			}
+			if err := db.Create(&book).Error; err != nil {
+				b.Errorf("Create book failed, expected %v, got %v", nil, err)
 			}
 		}
 	})
