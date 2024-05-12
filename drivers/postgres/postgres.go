9f699cf3dd99d01ba2bda3640a87783f162bf91a
Bart <bartventer@outlook.com>
2024-05-12 16:34:56 +0000

    dcos(drivers/postgres): add package level documentation

--- drivers/postgres/postgres.go
+++ drivers/postgres/postgres.go
@@ -1,51 +1,91 @@
 /*
-Package postgres provides a PostgreSQL driver for GORM, offering tools to facilitate the construction and management of multi-tenant applications.
-
-It enables the isolation of tenant data in separate PostgreSQL schemas.
+Package postgres provides a [PostgreSQL] driver for [GORM], offering tools to facilitate the construction and management of multi-tenant applications.
 
 Example:
 
+	package main
+
 	import (
-
-		"github.com/bartventer/gorm-multitenancy/v5"
-		"github.com/bartventer/gorm-multitenancy/v5/drivers/postgres"
 		"gorm.io/gorm"
-
+		"github.com/bartventer/gorm-multitenancy/v5/drivers/postgres"
+		"github.com/bartventer/gorm-multitenancy/v5/drivers/postgres/scopes"
 	)
 
-	func main() {
-		db, err := gorm.Open(postgres.Open("your connection string"), &gorm.Config{})
-		if err != nil {
-			panic(err)
-		}
-
-		// Register your tenant model
-		if err := postgres.RegisterModels(db, &Tenant{}); err != nil {
-			panic(err)
-		}
-
-		// Create public schema
-		if err := postgres.MigratePublicSchema(db); err != nil {
-			panic(err)
-		}
-
-		// Create a new tenant
-		tenant := Tenant{
-			TenantModel: postgres.TenantModel{
-				DomainURL:  "tenant1.example.com",
-				SchemaName: "tenant1",
-			},
-		}
-		if err := db.Create(&tenant).Error; err != nil {
-			panic(err)
-		}
-
-		// Create schema for the new tenant
-		postgres.CreateSchemaForTenant(db, tenant.SchemaName)
-
-		// Drop schema for the tenant
-		postgres.DropSchemaForTenant(db, tenant.SchemaName)
+	// Tenant is a public model
+	type Tenant struct {
+	    gorm.Model
+	    postgres.TenantModel // Embed the TenantModel
 	}
+
+	// Implement the gorm.Tabler interface
+	func (t *Tenant) TableName() string {return "public.tenants"} // Note the public. prefix
+
+	// Book is a tenant specific model
+	type Book struct {
+	    gorm.Model
+	    Title        string
+	    TenantSchema string `gorm:"column:tenant_schema"`
+	    Tenant       Tenant `gorm:"foreignKey:TenantSchema;references:SchemaName"`
+	}
+
+	// Implement the gorm.Tabler interface
+	func (b *Book) TableName() string {return "books"} // Note the lack of prefix
+
+	// Implement the TenantTabler interface
+	func (b *Book) IsTenantTable() bool {return true} // This classifies the model as a tenant specific model
+
+	func main(){
+		// Open a connection to the database
+	    db, err := gorm.Open(postgres.New(postgres.Config{
+	        DSN: "host=localhost user=postgres password=postgres dbname=postgres port=5432 sslmode=disable",
+	    }), &gorm.Config{})
+	    if err != nil {
+	        panic(err)
+	    }
+
+		// Register models
+	    if err := postgres.RegisterModels(db, &Tenant{}, &Book{}); err != nil {
+	        panic(err)
+	    }
+
+		// Migrate the public schema
+	    if err := postgres.MigratePublicSchema(db); err != nil {
+	        panic(err)
+	    }
+
+		// Create a tenant
+	    tenant := &Tenant{
+	        TenantModel: postgres.TenantModel{
+	            DomainURL: "tenant1.example.com",
+	            SchemaName: "tenant1",
+	        },
+	    }
+	    if err := db.Create(tenant).Error; err != nil {
+	        panic(err)
+	    }
+
+		// Create the schema for the tenant
+	    if err := postgres.CreateSchemaForTenant(db, tenant.SchemaName); err != nil {
+	        panic(err)
+	    }
+
+		// Create a book for the tenant
+		b := &Book{
+			Title: "Book 1",
+			TenantSchema: tenant.SchemaName,
+		}
+		if err := db.Scopes(scopes.WithTenantSchema(tenant.SchemaName)).Create(b).Error; err != nil {
+			panic(err)
+		}
+
+		// Drop the schema for the tenant
+	    if err := postgres.DropSchemaForTenant(db, tenant.SchemaName); err != nil {
+	        panic(err)
+	    }
+	}
+
+[PostgreSQL]: https://www.postgresql.org
+[GORM]: https://gorm.io
 */
 package postgres
 
