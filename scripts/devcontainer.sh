900c5736c7950ae3928b90afc31caffc20228369
Bart Venter <bartventer@outlook.com>
2024-06-21 08:34:58 +0200

    chore(devcontainer): Update image name [skip ci]

--- scripts/devcontainer.sh
+++ scripts/devcontainer.sh
@@ -3,14 +3,11 @@
 set -euo pipefail
 
 echo "🚀 Building devcontainer..."
-echo
-
 devcontainer build \
     --log-level debug \
     --workspace-folder .devcontainer/build \
-    --image-name ghcr.io/bartventer/gorm-multitenanacy/devcontainer:latest \
+    --image-name ghcr.io/bartventer/gorm-multitenancy/devcontainer:latest \
     --platform linux/amd64 \
     --push
 
-echo
 echo "🎉 OK. Successfully built devcontainer."
