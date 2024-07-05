31676bf55ab53d82bcc0e1de9831bcdec05afa0a
Bart Venter <bartventer@outlook.com>
2024-07-05 08:39:24 +0000

    refactor(postgres): Internalize Migration Options Handling
    BREAKING CHANGE: The `MigrationOptions` struct within the `postgres` driver has been removed from the public API and is now managed internally by the driver. This change streamlines the migration process and enhances the driver's internal logic, ensuring a more robust and maintainable architecture.

---
+++ postgres/internal/migrator/option.go
@@ -0,0 +1,50 @@
+// Package migrator provides utilities for database migration management.
+package migrator
+
+import (
+	"github.com/bartventer/gorm-multitenancy/v7/pkg/driver"
+	"gorm.io/gorm"
+)
+
+const pkgName = "gorm-multitenancy/postgres/internal/migrator"
+
+type key string
+
+const migratorKey key = pkgName + "/migrator"
+
+type option uint
+
+// Define values for [option].
+const (
+	DefaultOption option = iota
+	MigratorOption
+)
+
+// WithOption sets the migration option for the database.
+func WithOption(opt option) func(*gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		return db.Set(string(migratorKey), opt)
+	}
+}
+
+// OptionFromDB retrieves the migration option from the database.
+// If the option is not found or is not of the correct type, it returns [driver.ErrInvalidMigration].
+func OptionFromDB(db *gorm.DB) (option, error) {
+	o, optFound := db.Get(string(migratorKey))
+
+	if !optFound || o == nil {
+		return 0, driver.ErrInvalidMigration
+	}
+
+	optVal, ok := o.(option)
+	if !ok {
+		return 0, driver.ErrInvalidMigration
+	}
+
+	switch optVal {
+	case DefaultOption, MigratorOption:
+		return optVal, nil
+	default:
+		return 0, driver.ErrInvalidMigration
+	}
+}
