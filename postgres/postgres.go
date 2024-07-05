161fe8ea48e0fa79622bfea332548db2e42241ad
Bart Venter <bartventer@outlook.com>
2024-07-05 08:36:41 +0000

    refactor(postgres): Rename CreateSchemaForTenant to MigrateTenantModels
    BREAKING CHANGE: Both the standalone function and the method on the `postgres.Migrator` related to tenant schema creation have been renamed to `MigrateTenantModels`. This unified change better reflects their purpose and functionality, aligning with standard naming conventions and clarifying their intent within the driver module.
    
    **Updated Standalone Function Signature:**
    
    ```diff
    -func CreateSchemaForTenant(db *gorm.DB, schemaName string) error
    +func MigrateTenantModels(db *gorm.DB, schemaName string) error
    ```
    
    **Updated Method Signature on `postgres.Migrator`:**
    
    ```diff
    -func (m Migrator) CreateSchemaForTenant(tenant string) error
    +func (m Migrator) MigrateTenantModels(tenantID string) error
    ```
    
    > [!NOTE]
    > _The underlying logic for both the function and method remains unchanged, with only the names being updated to provide clarity and consistency across the driver module._

--- postgres/postgres.go
+++ postgres/postgres.go
@@ -1,244 +1,99 @@
 /*
-Package postgres provides a [PostgreSQL] driver for [GORM], offering tools to facilitate the construction and management of multi-tenant applications.
+Package postgres provides a [gorm.Dialector] implementation for [PostgreSQL] databases
+to support multitenancy in GORM applications, enabling tenant-specific operations
+and shared resources management. It includes utilities for registering models,
+migrating shared and tenant-specific models, and configuring the database for
+tenant-specific operations.
 
-Example:
+# Model Registration
 
-	package main
+To register models for multitenancy support, use [RegisterModels]. This should
+be done before running any migrations or tenant-specific operations.
 
-	import (
-		"gorm.io/gorm"
-		"github.com/bartventer/gorm-multitenancy/postgres/v7"
-		"github.com/bartventer/gorm-multitenancy/postgres/v7/scopes"
-	)
+# Migration Strategy
 
-	// Tenant is a public model
-	type Tenant struct {
-	    gorm.Model
-	    postgres.TenantModel // Embed the TenantModel
-	}
+To ensure data integrity and schema isolation across tenants, the AutoMigrate method
+has been disabled. Instead, use the provided shared and tenant-specific migration methods.
+[driver.ErrInvalidMigration] is returned if the migration method is called directly.
 
-	// Implement the gorm.Tabler interface
-	func (Tenant) TableName() string {return "public.tenants"} // Note the public. prefix
+# Shared Model Migrations
 
-	// Book is a tenant specific model
-	type Book struct {
-	    gorm.Model
-	    Title        string
-	    TenantSchema string `gorm:"column:tenant_schema"`
-	    Tenant       Tenant `gorm:"foreignKey:TenantSchema;references:SchemaName"`
-	}
+To migrate shared models, use [MigratePublicSchema].
 
-	// Implement the gorm.Tabler interface
-	func (Book) TableName() string {return "books"} // Note the lack of prefix
+# Tenant Model Migrations
 
-	// Implement the TenantTabler interface
-	func (Book) IsTenantTable() bool {return true} // This classifies the model as a tenant specific model
+To migrate tenant-specific models, use [MigrateTenantModels].
 
-	func main(){
-		// Open a connection to the database
-	    db, err := gorm.Open(postgres.New(postgres.Config{
-	        DSN: "host=localhost user=postgres password=postgres dbname=postgres port=5432 sslmode=disable",
-	    }), &gorm.Config{})
-	    if err != nil {
-	        panic(err)
-	    }
+# Tenant Offboarding
 
-		// Register models
-	    if err := postgres.RegisterModels(db, &Tenant{}, &Book{}); err != nil {
-	        panic(err)
-	    }
+To clean up the database for a removed tenant, use [DropSchemaForTenant].
 
-		// Migrate the public schema
-	    if err := postgres.MigratePublicSchema(db); err != nil {
-	        panic(err)
-	    }
+# Tenant Context Configuration
 
-		// Create a tenant
-	    tenant := &Tenant{
-	        TenantModel: postgres.TenantModel{
-	            DomainURL: "tenant1.example.com",
-	            SchemaName: "tenant1",
-	        },
-	    }
-	    if err := db.Create(tenant).Error; err != nil {
-	        panic(err)
-	    }
+To configure the database for operations specific to a tenant, use [schema.SetSearchPath].
 
-		// Create the schema for the tenant
-	    if err := postgres.CreateSchemaForTenant(db, tenant.SchemaName); err != nil {
-	        panic(err)
-	    }
+# Current Tenant Context
 
-		// Create a book for the tenant
-		b := &Book{
-			Title: "Book 1",
-			TenantSchema: tenant.SchemaName,
-		}
-		if err := db.Scopes(scopes.WithTenantSchema(tenant.SchemaName)).Create(b).Error; err != nil {
-			panic(err)
-		}
-
-		// Drop the schema for the tenant
-	    if err := postgres.DropSchemaForTenant(db, tenant.SchemaName); err != nil {
-	        panic(err)
-	    }
-	}
+To retrieve the identifier for the current tenant context, use [schema.CurrentSearchPath].
 
 [PostgreSQL]: https://www.postgresql.org
-[GORM]: https://gorm.io
 */
 package postgres
 
 import (
-	"fmt"
-	"strings"
-	"sync"
+	"context"
 
+	"github.com/bartventer/gorm-multitenancy/postgres/v7/schema"
+	multitenancy "github.com/bartventer/gorm-multitenancy/v7"
+	"github.com/bartventer/gorm-multitenancy/v7/pkg/driver"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"gorm.io/gorm/migrator"
 )
 
-type (
-	// Config is the configuration for the postgres driver.
-	Config = postgres.Config
+// DriverName specifies the PostgreSQL driver name, used for driver registration.
+var DriverName = postgres.Dialector{}.Name()
 
-	// Dialector is the postgres dialector with multitenancy support.
-	Dialector struct {
-		postgres.Dialector
-		*migratorConfig
-		rw *sync.RWMutex
-	}
-)
+var _ multitenancy.Adapter = new(postgresAdapter)
+var _ driver.DBFactory = new(postgresAdapter)
 
-// Check interface.
-var _ gorm.Dialector = (*Dialector)(nil)
+// postgresAdapter is a PostgreSQL-specific implementation of the [driver.DBFactory] interface.
+type postgresAdapter struct{}
 
-func initializeDialector(d *Dialector, models ...interface{}) {
-	mtc, err := newMigratorConfig(models)
-	if err != nil {
-		panic(err)
-	}
-	d.migratorConfig = mtc
+func init() { //nolint:gochecknoinits // Required for driver registration.
+	multitenancy.Register(DriverName, &postgresAdapter{})
 }
 
-// Open opens a connection to a PostgreSQL database using the provided DSN (Data Source Name) and models.
-func Open(dsn string, models ...interface{}) gorm.Dialector {
-	d := &Dialector{
-		Dialector: *postgres.Open(dsn).(*postgres.Dialector),
-		rw:        &sync.RWMutex{},
-	}
-	initializeDialector(d, models...)
-	return d
+// AdaptDB implements [multitenancy.Adapter].
+func (p *postgresAdapter) AdaptDB(ctx context.Context, db *gorm.DB) (*multitenancy.DB, error) {
+	return multitenancy.NewDB(&postgresAdapter{}, db), nil
 }
 
-// New creates a new PostgreSQL dialector with multitenancy support.
-func New(config Config, models ...interface{}) gorm.Dialector {
-	d := &Dialector{
-		Dialector: *postgres.New(config).(*postgres.Dialector),
-		rw:        &sync.RWMutex{},
-	}
-	initializeDialector(d, models...)
-	return d
+// MigrateSharedModels implements [driver.DBFactory].
+func (p *postgresAdapter) MigrateSharedModels(_ context.Context, db *gorm.DB) error {
+	return MigratePublicSchema(db)
 }
 
-// Migrator returns a [gorm.Migrator] implementation for the Dialector.
-func (dialector Dialector) Migrator(db *gorm.DB) gorm.Migrator {
-	dialector.rw.RLock()
-	defer dialector.rw.RUnlock()
-
-	return &Migrator{
-		postgres.Migrator{
-			Migrator: migrator.Migrator{
-				Config: migrator.Config{
-					DB:                          db,
-					Dialector:                   dialector,
-					CreateIndexAfterCreateTable: true,
-				},
-			},
-		},
-		&migratorConfig{
-			publicModels: dialector.publicModels,
-			tenantModels: dialector.tenantModels,
-		},
-		&sync.RWMutex{},
-	}
+// MigrateTenantModels implements [driver.DBFactory].
+func (p *postgresAdapter) MigrateTenantModels(_ context.Context, db *gorm.DB, tenantID string) error {
+	return MigrateTenantModels(db, tenantID)
 }
 
-// RegisterModels registers the given models with the provided [gorm.DB] instance for multitenancy support.
-func RegisterModels(db *gorm.DB, models ...interface{}) error {
-	dialector := db.Dialector.(*Dialector)
-	mtc, err := newMigratorConfig(models)
-	if err != nil {
-		return fmt.Errorf("failed to register models: %w", err)
-	}
-
-	dialector.rw.Lock()
-	dialector.migratorConfig = mtc
-	dialector.rw.Unlock()
-	return nil
+// OffboardTenant implements [driver.DBFactory].
+func (p *postgresAdapter) OffboardTenant(_ context.Context, db *gorm.DB, tenantID string) error {
+	return DropSchemaForTenant(db, tenantID)
 }
 
-// MigratePublicSchema migrates the public schema in the database.
-func MigratePublicSchema(db *gorm.DB) error {
-	return db.Migrator().(*Migrator).MigratePublicSchema()
+// RegisterModels implements [driver.DBFactory].
+func (p *postgresAdapter) RegisterModels(_ context.Context, db *gorm.DB, models ...driver.TenantTabler) error {
+	return RegisterModels(db, models...)
 }
 
-// CreateSchemaForTenant creates a new schema for a specific tenant in the PostgreSQL database.
-func CreateSchemaForTenant(db *gorm.DB, schemaName string) error {
-	return db.Migrator().(*Migrator).CreateSchemaForTenant(schemaName)
+// UseTenant implements [driver.DBFactory].
+func (p *postgresAdapter) UseTenant(_ context.Context, db *gorm.DB, tenantID string) (func() error, error) {
+	return schema.SetSearchPath(db, tenantID)
 }
 
-// DropSchemaForTenant drops the schema for a specific tenant in the PostgreSQL database (CASCADE).
-func DropSchemaForTenant(db *gorm.DB, schemaName string) error {
-	return db.Migrator().(*Migrator).DropSchemaForTenant(schemaName)
-}
-
-// newMigratorConfig creates a new multitenancy configuration based on the provided models.
-func newMigratorConfig(models []interface{}) (*migratorConfig, error) {
-	// It separates the models into public models and private models based on their table names.
-	// Public models are those that have a table name starting with the default schema name (PublicSchemaName),
-	// while private models are those that implement the TenantTabler interface and have a table name without a fullstop.
-	// If any model does not meet the required criteria, an error message is appended to the errStrings slice.
-	// If there are any errors, the function returns nil and an error. Otherwise, it returns a new multitenancyConfig
-	// containing the public models, private models, and all the models.
-	var (
-		publicModels  = make([]interface{}, 0, len(models))
-		privateModels = make([]interface{}, 0, len(models))
-		errStrings    = make([]string, 0)
-	)
-	for _, m := range models {
-		tn, ok := m.(interface{ TableName() string })
-		if !ok {
-			errStrings = append(errStrings, fmt.Sprintf("model %T does not implement TableName()", m))
-			continue
-		}
-		tt, ok := m.(TenantTabler)
-		parts := strings.Split(tn.TableName(), ".")
-		if ok && tt.IsTenantTable() {
-			// ensure that the private model does not contain a fullstop
-			if len(parts) > 1 {
-				errStrings = append(errStrings, fmt.Sprintf("invalid table name for model %T labeled as tenant table, table name should not contain a fullstop, got '%s'", m, tn.TableName()))
-				continue
-			}
-			privateModels = append(privateModels, m)
-		} else {
-			// ensure that the public model starts with the default schema (public.)
-			if len(parts) != 2 || parts[0] != PublicSchemaName {
-				errStrings = append(errStrings, fmt.Sprintf("invalid table name for model %T labeled as public table, table name should start with '%s.', got '%s'", m, PublicSchemaName, tn.TableName()))
-				continue
-			}
-			publicModels = append(publicModels, m)
-		}
-	}
-
-	// if there are errors, panic
-	if len(errStrings) > 0 {
-		return nil, fmt.Errorf("failed to create multitenancy config: %s", strings.Join(errStrings, "; "))
-	}
-
-	return &migratorConfig{
-		publicModels: publicModels,
-		tenantModels: privateModels,
-	}, nil
+// CurrentTenant implements [driver.DBFactory].
+func (p *postgresAdapter) CurrentTenant(_ context.Context, db *gorm.DB) string {
+	return schema.CurrentSearchPath(db)
 }
