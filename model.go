753679a8ab400f1d2d7447f619f58e663a1fc4e2
Bart Venter <bartventer@outlook.com>
2024-07-05 04:38:45 +0000

    refactor: Update Tenant Models
    BREAKING CHANGE: The relocation of `TenantModel` and `TenantPKModel` from the `postgres` driver to the `github.com/bartventer/gorm-multitenancy/v7` package, and the renaming of the `SchemaName` field to `ID` in `TenantPKModel`, necessitate updates to import paths and model references across all projects using the `gorm-multitenancy` package.
    
    - **Structs Relocation**: Moved `TenantModel` and `TenantPKModel` from the `postgres` driver to `github.com/bartventer/gorm-multitenancy/v7`, making them accessible across all drivers and promoting code reuse.
    - **Field Renaming**: Renamed `SchemaName` field to `ID` in `TenantPKModel` for a more generic and consistent primary key representation, aligning with standard naming conventions.
    - **Compatibility Update**: Modified check constraints for the `SchemaName` (now `ID`) field to ensure compatibility with all supported database drivers.
    
    Updated import paths:
    
    ```diff
    -github.com/bartventer/gorm-multitenancy/postgres/v7.TenantModel
    +github.com/bartventer/gorm-multitenancy/v7.TenantModel
    
    -github.com/bartventer/gorm-multitenancy/postgres/v7.TenantPKModel
    +github.com/bartventer/gorm-multitenancy/v7.TenantPKModel
    ```

---
+++ model.go
@@ -0,0 +1,49 @@
+package multitenancy
+
+// TenantModel a basic GoLang struct which includes the following fields: DomainURL, SchemaName.
+// It's intended to be embedded into any public model that needs to be scoped to a tenant, supporting both PostgreSQL and MySQL.
+// It may be embedded into your model or you may build your own model without it.
+//
+// For example:
+//
+//	type Tenant struct {
+//	  multitenancy.TenantModel
+//	}
+type TenantModel struct {
+	// DomainURL is the domain URL of the tenant
+	DomainURL string `json:"domainURL" mapstructure:"domainURL" gorm:"column:domain_url;uniqueIndex;size:128"`
+
+	// SchemaName is the schema name of the tenant.
+	//
+	// Field-level permissions are restricted to read and create.
+	//
+	// The following constraints are applied:
+	// 	- unique index
+	// 	- size: 63
+	//  - check: Not less than 3 characters long
+	//
+	// Note: Due to differences in regular expression support between PostgreSQL and MySQL,
+	// complex validations based on patterns (e.g., ensuring the schema name does not start with 'pg_')
+	// should be enforced at the application level or through database-specific features.
+	//
+	// Examples of valid schema names:
+	// 	- "tenant1"
+	// 	- "_tenant"
+	// 	- "tenant_1"
+	//
+	// Examples of invalid schema names:
+	// 	- "t" (less than 3 characters long)
+	// 	- "tenant1!" (contains special characters)
+	//
+	SchemaName string `json:"schemaName" mapstructure:"schemaName" gorm:"column:schema_name;uniqueIndex;->;<-:create;size:63;check:LENGTH(schema_name) >= 3"`
+}
+
+// TenantPKModel is identical to [TenantModel] but with SchemaName as a primary key field.
+type TenantPKModel struct {
+	// DomainURL is the domain URL of the tenant
+	DomainURL string `json:"domainURL" mapstructure:"domainURL" gorm:"column:domain_url;uniqueIndex;size:128"`
+
+	// SchemaName is the schema name of the tenant and the primary key of the model.
+	// For details on the constraints and rules for this field, see [TenantModel.SchemaName].
+	ID string `json:"id" mapstructure:"id" gorm:"column:id;primaryKey;uniqueIndex;->;<-:create;size:63;check:LENGTH(id) >= 3"`
+}
