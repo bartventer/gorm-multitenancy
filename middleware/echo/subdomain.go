b98e183c0a2cd75ee85660d9f1fec265da305861
Bart Venter <bartventer@outlook.com>
2024-07-05 09:11:52 +0000

    feat(middlware/echo): Add alias for ExtractSubdomain from nethttpmw package

---
+++ middleware/echo/subdomain.go
@@ -0,0 +1,6 @@
+package echo
+
+import nethttpmw "github.com/bartventer/gorm-multitenancy/middleware/nethttp/v7"
+
+// ExtractSubdomain is an alias for [nethttpmw.ExtractSubdomain].
+var ExtractSubdomain = nethttpmw.ExtractSubdomain
