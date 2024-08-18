8d4867b3149ef85f09846799d0e859ffc7f8b330
Bart Venter <bartventer@outlook.com>
2024-08-18 21:44:26 +0000

    chore(pkg/driver): Update ErrInvalidMigration error message
    Refactor the ErrInvalidMigration error message to provide clearer instructions on using MigrateSharedModels or MigrateTenantModels instead of calling AutoMigrate directly.

--- pkg/driver/errors.go
+++ pkg/driver/errors.go
@@ -6,8 +6,8 @@ import (
 
 var (
 	// ErrInvalidMigration is returned when an invalid migration is detected.
-	ErrInvalidMigration = errors.New(`
-		Invalid migration. Please ensure you're using MigrateSharedModels or MigrateTenantModels
-		instead of calling AutoMigrate directly.
-	`)
+	ErrInvalidMigration = errors.Join(
+		errors.New("invalid migration"),
+		errors.New("please ensure you're using MigrateSharedModels or MigrateTenantModels instead of calling AutoMigrate directly"),
+	)
 )
